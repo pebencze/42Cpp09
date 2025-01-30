2789d690ec45b6bef214dba90ed447885c918a65
Petra Bencze <petra.bncz@gmail.com>
2025-01-30 18:36:37 +0100

    started ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,33 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <ctime>
+
+int main(int ac, char** av) {
+    std::fstream                inputFile(av[1]);
+    std::fstream                csvFile("../data.csv");
+    std::map<struct tm, int>    map;
+    std::map<struct tm, int>    data;
+    std::string                 line;
+
+    if (ac != 2) {
+        std::cerr << "Error: argument required." << std::endl;
+        return 1;
+    }
+
+    if (!inputFile.is_open() || !csvFile.is_open()) {
+        std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    getline(inputFile, line)
+    while (getline(inputFile, line)) {
+        map = BitcoinExchange::parseMap(line);
+    }
+    BitcoinExchange::calculate();
+
+    inputFile.close();
+    csvFile.close();
+
+    return 0;
+}
