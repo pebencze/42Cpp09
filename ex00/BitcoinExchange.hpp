212c935a3515446d49e4e83fbbb998ace866b3c2
pebencze <petra.bncz@gmail.com>
2025-02-06 16:00:27 +0100

    parseInput done

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -10,6 +10,7 @@ class BitcoinExchange {
 		BitcoinExchange & operator=(BitcoinExchange const & src);
 
         parseMap(std::ifstream &inputFile);
+		void checkDate(std::string date);
         void calculate();
 
     private:
@@ -35,8 +36,33 @@ BitcoinExchange & BitcoinExchange::operator=(BitcoinExchange const & src) {
 	return *this;
 }
 
+BitcoinExchange::checkDate(std::string date) {
+	std::time_t t = std::time(0);
+	std::tm* now = std::localtime(&t);
+	std::string year = std::to_string(now->tm_year + 1900);
+	std::string month = std::to_string(now->tm_mon + 1);
+	std::string day = std::to_string(now->tm_mday);
+	int dateInt = atoi(date.substr(0, 4) + date.substr(5, 2) + date.substr(8, 2));
 
-BitcoinExchange::parseMap(std::ifstream &inputFile) {
+	if (atoi(year + month + day) < dateInt) {
+		throw std::invalid_argument("Error: future date =>" + date);
+	}
+	if (date.length() != 10 || date.find_first_not_of("0123456789-") != std::string::npos
+		|| (date[4] != '-' || date[7] != '-')) {
+		throw std::invalid_argument("Error: bad date format =>" + date);
+	}
+	if (atoi(date.substr(0, 4)) > (now->tm_year + 1900)) {
+		throw std::invalid_argument("Error: bad year =>" + date);
+	}
+	if (atoi(date.substr(5, 2)) > 12 || atoi(date.substr(5, 2)) < 1) {
+		throw std::invalid_argument("Error: bad month =>" + date);
+	}
+	if (atoi(date.substr(8, 2)) > 31 || atoi(date.substr(8, 2)) < 1) {
+		throw std::invalid_argument("Error: bad day =>" + date);
+	}
+}
+
+BitcoinExchange::parseIn(std::ifstream &inputFile) {
     std::string date;
 	std::string line;
 
@@ -45,7 +71,7 @@ BitcoinExchange::parseMap(std::ifstream &inputFile) {
 		std::cerr << "Error: missing header\n";
 		return ;
 	}
-    while (getline(inputFile, line)) {
+	while (getline(inputFile, line)) {
 		std::stringstream ss(line);
 		std::string date, valueStr;
 		double value;
@@ -60,13 +86,13 @@ BitcoinExchange::parseMap(std::ifstream &inputFile) {
 				if (errno = ERANGE || value < 0 || value > 1000) {
 					throw std::out_of_range("Error: value out of range 0 - 1000 =>" + valueStr);
 				}
-    			input.emplace(date, value);
+	 			input.emplace(date, value);
 			} else {
 				 throw std::invalid_argument("Error: bad input =>" + line);
 			}
 		} catch (std::exception & e) {
 			std::cerr << e.what() << std::endl;
 		}
-    }
+	}
 
 }
